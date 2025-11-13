1c806d07903f1ae8adbd14eb99505c36229d2fd4
rud15dns <rud15dns@naver.com>
2025-11-13 23:10:12 +0900

    feat: 500 에러 시 디스코드 알림 구현

--- 따밥/server/src/main/java/umc/server/global/apiPayload/handler/GeneralExceptionAdvice.java
+++ 따밥/server/src/main/java/umc/server/global/apiPayload/handler/GeneralExceptionAdvice.java
@@ -1,26 +1,70 @@
 package umc.server.global.apiPayload.handler;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import umc.server.domain.test.service.DiscordClient;
+import umc.server.domain.test.service.DiscordEmbedMessage;
 import umc.server.global.apiPayload.ApiResponse;
 import umc.server.global.apiPayload.code.BaseErrorCode;
 import umc.server.global.apiPayload.code.GeneralErrorCode;
 import umc.server.global.apiPayload.exception.GeneralException;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestControllerAdvice
 public class GeneralExceptionAdvice {
+    @Value("${discord.webhook.url}")
+    private String webhookUrl;
+    private final DiscordClient discordClient;
+
+    public GeneralExceptionAdvice(DiscordClient discordClient) {
+        this.discordClient = discordClient;
+    }
     // 애플리케이션에서 발생하는 커스텀 예외를 처리.
     @ExceptionHandler(GeneralException.class)
     public ResponseEntity<ApiResponse<Void>> handleException(
             GeneralException ex
     ){
+        if (ex.getCode().getStatus() == HttpStatus.INTERNAL_SERVER_ERROR) {
+            return handleInternalServerError(ex);
+        }
         return ResponseEntity.status(ex.getCode().getStatus())
                 .body(ApiResponse.onFailure(
                                 ex.getCode(),
                                 null
                         )
                 );
+    }
+
+    private ResponseEntity<ApiResponse<Void>> handleInternalServerError(GeneralException ex) {
+        // Discord Webhook
+        DiscordEmbedMessage message = buildDiscordEmbedMessage(ex);
+        discordClient.send(webhookUrl, message);
+
+        return ResponseEntity.status(ex.getCode().getStatus())
+                .body(ApiResponse.onFailure(
+                                ex.getCode(),
+                                null
+                        )
+                );
+    }
+
+    private DiscordEmbedMessage buildDiscordEmbedMessage(GeneralException ex){
+        String content = String.format(
+                " **500에러 발생**\n\n" +
+                        "**시각:** %S\n" +
+                        "**에러 코드:** %s\n" +
+                        "**에러 메시지:** %s",
+                LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                ex.getCode(),
+                ex.getMessage()
+        );
+
+        return new DiscordEmbedMessage(content);
     }
 
     // 그 외의 정의되지 않은 모든 예외 처리
